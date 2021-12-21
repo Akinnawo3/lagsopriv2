3b494cfb3fc5cb8db9b0015fe08c0e06bfdec8a3
Increase Nkanta <engr.increase@gmail.com>
2021-12-21 13:17:50 +0100

    added firebase file

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
+require('dotenv').config();
+
+firebase.initializeApp({
+  messagingSenderId: process.env.REACT_APP_MEASUREMENT_ID,
+})
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function (registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function (err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
+
+const initMessaging = firebase.messaging()
