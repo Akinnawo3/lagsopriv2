91b789632f817dc186cee18e31fc9f1b9163ef17
Akinnawo3 <Akinnawovictor@gmail.com>
2022-02-24 11:42:45 +0100

    sidebar rearrangement

---
+++ src/routes/vehicles/exponent.py
@@ -0,0 +1,12 @@
+
+
+# Write a python code that concepts three integers a, b and c. with the code, find the value of 
+# a4  + b3 + c2
+
+
+# accepts 3 integers
+a= int(input("enter the value of a: "))
+b= int(input("enter the value of b: "))
+c= int(input("enter the value of c: "))
+result =  (a**4) + (b**3) + (c**2)
+print(result)
