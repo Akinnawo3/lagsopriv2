264d4e9180784b4875b1c1bf563a29bc58302a76
Akinnawo3 <Akinnawovictor@gmail.com>
2022-02-28 06:13:26 +0100

    line graph component

--- src/routes/maps/component/downloadsChart.js
+++ src/routes/maps/component/downloadsChart.js
@@ -1,33 +1,43 @@
 import React from "react";
 import RctCollapsibleCard from "Components/RctCollapsibleCard/RctCollapsibleCard";
 import {Line} from "react-chartjs-2";
+import {connect} from "react-redux";
 
 const DownloadsChart = () => {
   const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [
-      {
-        label: "First dataset",
-        data: [33, 53, 85, 41, 44, 65],
-        fill: true,
-        backgroundColor: "rgba(75,192,192,0.2)",
-        borderColor: "rgba(75,192,192,1)",
-      },
       // {
-      //   label: "Second dataset",
-      //   data: [33, 25, 35, 51, 54, 76],
-      //   fill: false,
-      //   borderColor: "#742774",
+      //   label: "First dataset",
+      //   data: [33, 53, 85, 41, 44, 65],
+      //   fill: true,
+      //   backgroundColor: "rgba(75,192,192,0.2)",
+      //   borderColor: "rgba(75,192,192,1)",
       // },
+      {
+        label: "Second dataset",
+        data: [0, 2000, 125, 2400, 21],
+        fill: false,
+        tension: 0,
+        borderColor: "#742774",
+      },
     ],
   };
   return (
     <RctCollapsibleCard heading="Downloads">
-      <div >
+      <div>
         <Line data={data} />
       </div>
     </RctCollapsibleCard>
   );
 };
 
-export default DownloadsChart;
+const mapDispatchToProps = (dispatch) => ({
+  getDownloadsByArea: (spinner) => dispatch(getDownloadsByArea(spinner)),
+});
+const mapStateToProps = (state) => ({
+  downloadsByArea: state.users.downloadsByArea,
+  loading: state.loading.loading,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DownloadsChart);
