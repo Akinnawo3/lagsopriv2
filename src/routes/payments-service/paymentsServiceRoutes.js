6e395b49150d7531f659c617378ca2385a745129
Akinnawo3 <Akinnawovictor@gmail.com>
2022-03-02 09:43:14 +0100

    pending payment service table

--- src/routes/payments-service/paymentsServiceRoutes.js
+++ src/routes/payments-service/paymentsServiceRoutes.js
@@ -2,26 +2,28 @@
  * Payment Service DriverRoutes
  */
 /* eslint-disable */
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import {Route, Switch} from "react-router-dom";
 
 // async components
 import {
-    AsyncPaymentsService,
-    AsyncPaymentsServiceDetails,
-    AsyncPaymentsServiceSuccessful,
-    AsyncPaymentsServiceUnsuccessful,
-} from 'Components/AsyncComponent/AsyncComponent';
+  AsyncPaymentsService,
+  AsyncPaymentsServiceDetails,
+  AsyncPaymentsServiceSuccessful,
+  AsyncPaymentsServiceUnsuccessful,
+  AsyncPaymentsServicePending,
+} from "Components/AsyncComponent/AsyncComponent";
 
-const PaymentsServiceRoutes = ({ match }) => (
-    <div className="content-wrapper">
-        <Switch>
-            <Route path={`/admin/payments-service/unsuccessful`} component={AsyncPaymentsServiceUnsuccessful} />
-            <Route path={`/admin/payments-service/successful`} component={AsyncPaymentsServiceSuccessful} />
-            <Route path={`/admin/payments-service/:id`} component={AsyncPaymentsServiceDetails} />
-            <Route path={`/admin/payments-service`} component={AsyncPaymentsService} />
-        </Switch>
-    </div>
+const PaymentsServiceRoutes = ({match}) => (
+  <div className="content-wrapper">
+    <Switch>
+      <Route path={`/admin/payments-service/unsuccessful`} component={AsyncPaymentsServiceUnsuccessful} />
+      <Route path={`/admin/payments-service/successful`} component={AsyncPaymentsServiceSuccessful} />
+      <Route path={`/admin/payments-service/pending`} component={AsyncPaymentsServicePending} />
+      <Route path={`/admin/payments-service/:id`} component={AsyncPaymentsServiceDetails} />
+      <Route path={`/admin/payments-service`} component={AsyncPaymentsService} />
+    </Switch>
+  </div>
 );
 
 export default PaymentsServiceRoutes;
---
+++ src/routes/payments-service/pendingPaymentService.js
@@ -0,0 +1,47 @@
+/**
+ * Payments
+ */
+import React, {useState, useEffect} from "react";
+import PageTitleBar from "Components/PageTitleBar/PageTitleBar";
+import {connect} from "react-redux";
+import {getPayments, getPaymentsCount, getPaymentsService, getPaymentsServiceCount, getPaymentsServiceBalance} from "Actions/paymentAction";
+import PaymentServiceTable from "Routes/payments-service/component/paymentServiceTable";
+const qs = require("qs");
+
+const PendingPaymentService = ({history, match, getPayments, getPaymentsCount, payments, paymentsCount, getPaymentsServiceBalance}) => {
+  const pageFromQuery = qs.parse(history.location.search, {ignoreQueryPrefix: true}).page;
+  const [currentPage, setCurrentPage] = useState(() => {
+    return pageFromQuery === undefined ? 1 : parseInt(pageFromQuery, 10);
+  });
+  useEffect(() => {
+    if (pageFromQuery === undefined || payments.length < 1) {
+      getPayments(currentPage, 0, "", true);
+      getPaymentsCount(0);
+      getPaymentsServiceBalance(0);
+    }
+  }, []);
+
+  return (
+    <div className="table-wrapper">
+      <PageTitleBar title={"Pending Payments"} match={match} />
+      <PaymentServiceTable status={0} header={"Pending Payments"} />
+    </div>
+  );
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getPayments: (pageNo, transaction_status, auth_id, loading) => dispatch(getPaymentsService(pageNo, transaction_status, auth_id, loading)),
+    getPaymentsCount: (transaction_status, auth_id) => dispatch(getPaymentsServiceCount(transaction_status, auth_id)),
+    getPaymentsServiceBalance: (transaction_status, auth_id, payment_type) => dispatch(getPaymentsServiceBalance(transaction_status, auth_id, payment_type)),
+  };
+}
+
+const mapStateToProps = (state) => ({
+  payments: state.payments.paymentsService,
+  paymentsCount: state.payments.paymentsServiceCount,
+  isLoading: state.loading.loading,
+  sosUserDetails: state.sos.sosUserDetails,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PendingPaymentService);
