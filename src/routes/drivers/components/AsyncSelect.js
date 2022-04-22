d67689d49e155ae9cd3f1881e8f34a0a3799d840
Increase Nkanta <engr.increase@gmail.com>
2022-04-22 01:34:15 +0100

    fixed timeout issue

--- src/routes/drivers/components/AsyncSelect.js
+++ src/routes/drivers/components/AsyncSelect.js
@@ -1,46 +1,49 @@
 import AsyncSelect from "react-select/async";
 import axios from "axios";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import api from "../../../environments/environment";
-import {debounce} from "lodash";
+import { debounce } from "lodash";
 import useDebounce from "../../../helpers/debounceHelper";
 
-const AsyncSelectComponent = ({onChange}) => {
-  const [debouncer] = useDebounce();
-  const [value, setValue] = useState("");
-  const itemChange = (item) => {
-    console.log({item});
-    setValue(item);
-    onChange(item);
-    return item;
-  };
+const AsyncSelectComponent = ({ onChange }) => {
+   const [debouncer] = useDebounce();
+   const [value, setValue] = useState("");
 
-  const searchVehicle = async (data, callback) => {
-    try {
-      const res = await axios.get(`${api.vehicles}/v1.1/admin/vehicles?q=${data}&assign=${0}`);
-      console.log(res);
-      return res.data.data.map((item) => ({
-        value: item,
-        label: `${item.car_number_plate} - ${item.car_make} - ${item.car_model} (${item.car_color})`,
-      }));
-    } catch (err) {
-      console.log(err);
-    }
-  };
+   const itemChange = (item) => {
+      //  console.log({item});
+      setValue(item);
+      onChange(item);
+      return item;
+   };
 
-  const promiseOptions = (inputValue) =>
-    new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(searchVehicle(inputValue));
-      }, 1000);
-    });
+   const searchVehicle = async (data, callback) => {
+      try {
+         const res = await axios.get(`${api.vehicles}/v1.1/admin/vehicles?q=${data}&assign=${0}`);
+         console.log(res);
+         return res.data.data.map((item) => ({
+            value: item,
+            label: `${item.car_number_plate} - ${item.car_make} - ${item.car_model} (${item.car_color})`,
+         }));
+      } catch (err) {
+         console.log(err);
+      }
+   };
 
-  return (
-    <div>
-      <div>Select vehicle</div>
-      <AsyncSelect value={value} cacheOptions defaultOptions loadOptions={promiseOptions} onChange={itemChange} placeholder="Search by colour, plate number, model or year " />
-    </div>
-  );
+   let timeOutSearch = React.useRef() //for holding timeout when request is initialize
+   const promiseOptions = (inputValue) =>
+      new Promise((resolve) => {
+         clearTimeout(timeOutSearch.current) //clear any pending timeout before creating a new one
+         timeOutSearch.current = setTimeout(() => {
+            resolve(searchVehicle(inputValue));
+         }, 1000);
+      });
+
+   return (
+      <div>
+         <div>Select vehicle</div>
+         <AsyncSelect value={value} cacheOptions defaultOptions loadOptions={promiseOptions} onChange={itemChange} placeholder="Search by colour, plate number, model or year " />
+      </div>
+   );
 };
 
 export default AsyncSelectComponent;
