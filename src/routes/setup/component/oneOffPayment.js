84e16010e6c872483f18f9065af9f0a35dee7c57
Akinnawo3 <Akinnawovictor@gmail.com>
2022-07-13 15:09:10 +0100

    FF

--- src/routes/setup/component/oneOffPayment.js
+++ src/routes/setup/component/oneOffPayment.js
@@ -43,6 +43,8 @@ const OneOffPayment = ({match, getCustomerCare, customerCareNumbers, createCusto
   const updateOneOffPayment = async (e) => {
     e.preventDefault();
     setBreakDownModalOpen(false);
+    setBreakDownModalOpenLoan(false);
+
     const general_payload = {
       cost_of_asset: "",
       insurance,
@@ -54,29 +56,29 @@ const OneOffPayment = ({match, getCustomerCare, customerCareNumbers, createCusto
     };
     const general_payload_loan = {
       cost_of_asset: "",
-      insuranceLoan,
-      dashcamLoan,
-      soft_skills_plus_others_loan: softSkillsPlusOthers_Loan,
-      lasdriLoan,
+      insurance_loan: insuranceLoan,
+      dashcam_loan: dashcamLoan,
+      soft_skills_plus_others_loan: softSkillsPlusOthersLoan,
+      lasdri_loan: lasdriLoan,
       dms_subscription_loan: dmsSubLoan,
       e_taxi_sub_loan: eTaxiLoan,
     };
 
-        // to update for commercial driver take note of the cost_of_asset (thets were the ajor difference is)
-        // NowNon-Loan Drivers
-        await createCommercialDriverFee({
-          ...general_payload,
-          cost_of_asset: costAssetComm,
-          total:
-            stringToNumber(costAssetComm) +
-            stringToNumber(insurance) +
-            stringToNumber(dashcam) +
-            stringToNumber(softSkillsPlusOthers) +
-            stringToNumber(lasdri) +
-            stringToNumber(dmsSub) +
-            stringToNumber(eTaxi),
-        });
-      };
+    // to update for commercial driver take note of the cost_of_asset (thets were the ajor difference is)
+    // NowNon-Loan Drivers
+    await createCommercialDriverFee({
+      ...general_payload,
+      cost_of_asset: costAssetComm,
+      total:
+        stringToNumber(costAssetComm) +
+        stringToNumber(insurance) +
+        stringToNumber(dashcam) +
+        stringToNumber(softSkillsPlusOthers) +
+        stringToNumber(lasdri) +
+        stringToNumber(dmsSub) +
+        stringToNumber(eTaxi),
+    });
+
     // to update for socail driver take note of the cost_of_asset (thets where the major difference is)
     //Now Loan Drivers
     await createSocialDriverFee({
@@ -91,18 +93,42 @@ const OneOffPayment = ({match, getCustomerCare, customerCareNumbers, createCusto
         stringToNumber(dmsSubLoan) +
         stringToNumber(eTaxiLoan),
     });
+    // console.log({
+    //   ...general_payload_loan,
+    //   cost_of_asset: costAssetSoc,
+    //   total:
+    //     stringToNumber(costAssetSoc) +
+    //     stringToNumber(insuranceLoan) +
+    //     stringToNumber(dashcamLoan) +
+    //     stringToNumber(softSkillsPlusOthersLoan) +
+    //     stringToNumber(lasdriLoan) +
+    //     stringToNumber(dmsSubLoan) +
+    //     stringToNumber(eTaxiLoan),
+    // });
+  };
 
+  const openBreakDownModal = (type) => {
+    type === "non-loan" && setBreakDownModalOpen(true);
+    type === "loan" && setBreakDownModalOpenLoan(true);
+    console.log(customerCareNumbers?.com_driver_fee);
 
-  const openBreakDownModal = () => {
-    setBreakDownModalOpen(true);
     setCostAssetComm(customerCareNumbers?.com_driver_fee?.cost_of_asset);
     setCostAssetSoc(customerCareNumbers?.soc_driver_fee?.cost_of_asset);
+
+    // non loan extras
     setInsurance(customerCareNumbers?.com_driver_fee?.insurance);
     setDashcam(customerCareNumbers?.com_driver_fee?.dashcam);
     setSoftSkillsPlusOthers(customerCareNumbers?.com_driver_fee?.soft_skills_plus_others);
     setLasdri(customerCareNumbers?.com_driver_fee?.lasdri);
     setDmsSub(customerCareNumbers?.com_driver_fee?.dms_subscription);
     setEtaxi(customerCareNumbers?.com_driver_fee?.e_taxi_sub);
+    //  loan extras
+    setInsuranceLoan(customerCareNumbers?.soc_driver_fee?.insurance_loan);
+    setDashcamLoan(customerCareNumbers?.soc_driver_fee?.dashcam_loan);
+    setSoftSkillsPlusOthersLoan(customerCareNumbers?.soc_driver_fee?.soft_skills_plus_others_loan);
+    setLasdriLoan(customerCareNumbers?.soc_driver_fee?.lasdri_loan);
+    setDmsSubLoan(customerCareNumbers?.soc_driver_fee?.dms_subscription_loan);
+    setEtaxiLoan(customerCareNumbers?.soc_driver_fee?.e_taxi_sub_loan);
   };
 
   useEffect(() => {
@@ -111,50 +137,33 @@ const OneOffPayment = ({match, getCustomerCare, customerCareNumbers, createCusto
 
   return (
     <div className="row">
-      <div className="col col-xs-12 col-md-10">
+      <div className="col col-xs-12">
         <RctCollapsibleCard heading="One-off Payment" style={{minHeight: "70vh"}}>
           <div className="px-2 mt-1">
             <div className="row">
-              <div className="col col-sm-12 col-md-4 mr-3 d-flex flex-column">
-                <Card className="text-primary bg-light p-2 mb-3">
-                  <CardBody className="pb-0">
-                    <div className="text-value text-left text-muted fw-bold">Drivers fee </div>
-                  </CardBody>
-                  <div className="chart-wrapper mx-3 d-flex align-items-center justify-content-between" style={{height: "70px"}}>
-                    <span className="pr-2 font-xl" style={{fontSize: "2.5rem"}}>
-                      {`₦${customerCareNumbers?.com_driver_fee?.total.toLocaleString()}`}
-                    </span>
-                  </div>
-                </Card>
-                <Card className="text-primary bg-light p-2">
-                  <CardBody className="pb-0">
-                    <div className="text-value text-muted fw-bold">Loan Drivers Fee</div>
-                  </CardBody>
-                  <div className="chart-wrapper mx-3 d-flex align-items-center  justify-content-between" style={{height: "70px"}}>
-                    <span className=" font-xl" style={{fontSize: "2.5rem"}}>
-                      {`₦${customerCareNumbers?.soc_driver_fee?.total.toLocaleString()}`}
-                    </span>
-                  </div>
-                </Card>
-              </div>
+              {/* <div className="col col-sm-12 col-md-4 mr-3 d-flex flex-column"></div> */}
 
-              <div className=" col-sm-12 col-md-7 ">
+              <div className=" col-sm-12 col-md-6">
                 <div>
-                  <h3 className="fw-bold">Breakdown</h3>
+                  <h3 className="fw-bold">Breakdown (Non-Loan Driver)</h3>
+                  <Card className="text-primary bg-light p-2 mb-3">
+                    <CardBody className="pb-0">
+                      <div className="text-value text-left text-muted fw-bold">Total Drivers fee </div>
+                    </CardBody>
+                    <div className="chart-wrapper mx-3 d-flex align-items-center justify-content-between" style={{height: "70px"}}>
+                      <span className="pr-2 font-xl" style={{fontSize: "2.5rem"}}>
+                        {`₦${customerCareNumbers?.com_driver_fee?.total.toLocaleString()}`}
+                      </span>
+                    </div>
+                  </Card>
                   <div className="text-muted">
                     Percentege of Cost of Asset
                     <ul className="list-group">
                       <li className="list-group-item text-right">
                         <span className="pull-left">
-                          <div>Commercial Driver </div>
+                          <div> Non-Loan Driver </div>
                         </span>
                         <strong>{customerCareNumbers?.com_driver_fee?.cost_of_asset}</strong>
-                      </li>
-                      <li className="list-group-item text-right">
-                        <span className="pull-left">
-                          <div>Social Driver </div>
-                        </span>
-                        <strong>{customerCareNumbers?.soc_driver_fee?.cost_of_asset}</strong>
                       </li>
                     </ul>
                   </div>
@@ -205,11 +214,87 @@ const OneOffPayment = ({match, getCustomerCare, customerCareNumbers, createCusto
                       >
                         Add Parameter
                       </button> */}
-                      <button className="btn border-info" onClick={() => verifyUserPermssion("create_setup", () => openBreakDownModal(true))}>
+                      <button className="btn border-info my-3" onClick={() => verifyUserPermssion("create_setup", () => openBreakDownModal("non-loan"))}>
                         Edit Breakdown (Non-Loan)
                       </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-                      <button className="btn border-info btn-info ml-3 " onClick={() => verifyUserPermssion("create_setup", () => setBreakDownModalOpenLoan(true))}>
+              <div className=" col-sm-12 col-md-6">
+                <div>
+                  <h3 className="fw-bold">Breakdown (Loan Drivers)</h3>
+                  <Card className="text-primary bg-light p-2 mb-3">
+                    <CardBody className="pb-0">
+                      <div className="text-value text-muted fw-bold">Total Loan Drivers Fee</div>
+                    </CardBody>
+                    <div className="chart-wrapper mx-3 d-flex align-items-center  justify-content-between" style={{height: "70px"}}>
+                      <span className=" font-xl" style={{fontSize: "2.5rem"}}>
+                        {`₦${customerCareNumbers?.soc_driver_fee?.total.toLocaleString()}`}
+                      </span>
+                    </div>
+                  </Card>
+                  <div className="text-muted">
+                    Percentege of Cost of Asset
+                    <ul className="list-group">
+                      <li className="list-group-item text-right">
+                        <span className="pull-left">
+                          <div>Social Driver </div>
+                        </span>
+                        <strong>{customerCareNumbers?.soc_driver_fee?.cost_of_asset}</strong>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="text-muted mt-3">
+                    Others
+                    <ul className="list-group">
+                      <li className="list-group-item text-right">
+                        <span className="pull-left">
+                          <div>Registration and Insurance </div>
+                        </span>
+                        <strong>{customerCareNumbers?.com_driver_fee?.insurance_loan}</strong>
+                      </li>
+                      <li className="list-group-item text-right">
+                        <span className="pull-left">
+                          <div>Dashcam 1st month rent </div>
+                        </span>
+                        <strong>{customerCareNumbers?.com_driver_fee?.dashcam_loan}</strong>
+                      </li>
+                      <li className="list-group-item text-right">
+                        <span className="pull-left">
+                          <div>Soft Skill training,medicals,security check..</div>
+                        </span>
+                        <strong>{customerCareNumbers?.com_driver_fee?.soft_skills_plus_others_loan}</strong>
+                      </li>
+                      <li className="list-group-item text-right">
+                        <span className="pull-left">
+                          <div> LASDRI </div>
+                        </span>
+                        <strong>{customerCareNumbers?.com_driver_fee?.lasdri_loan}</strong>
+                      </li>
+                      <li className="list-group-item text-right">
+                        <span className="pull-left">
+                          <div> DMS Subscription </div>
+                        </span>
+                        <strong>{customerCareNumbers?.com_driver_fee?.dms_subscription_loan}</strong>
+                      </li>
+                      <li className="list-group-item text-right">
+                        <span className="pull-left">
+                          <div> E-taxi License </div>
+                        </span>
+                        <strong>{customerCareNumbers?.com_driver_fee?.e_taxi_sub_loan}</strong>
+                      </li>
+                    </ul>
+                    <div classsName="d-flex mt-2">
+                      {/* <button
+                        className="btn border-info mr-3"
+                        onClick={() => setParameterModalOpen(true)}
+                      >
+                        Add Parameter
+                      </button> */}
+
+                      <button className="btn border-info btn-info mt-3 " onClick={() => verifyUserPermssion("create_setup", () => openBreakDownModal("loan"))}>
                         Edit Breakdown (Loan)
                       </button>
                     </div>
@@ -283,27 +368,27 @@ const OneOffPayment = ({match, getCustomerCare, customerCareNumbers, createCusto
             <small className="fw-bold mt-3">Others</small>
             <FormGroup>
               <Label for="lastName">Registration and Insurance</Label>
-              <Input type="text" name="name" value={insurance} onChange={(e) => setInsurance(e.target.value)} required />
+              <Input type="text" name="name" value={insuranceLoan} onChange={(e) => setInsuranceLoan(e.target.value)} required />
             </FormGroup>
             <FormGroup>
               <Label for="lastName">Dashcam 1st month rent</Label>
-              <Input type="text" name="number" value={dashcam} onChange={(e) => setDashcam(e.target.value)} required />
+              <Input type="text" name="number" value={dashcamLoan} onChange={(e) => setDashcamLoan(e.target.value)} required />
             </FormGroup>
             <FormGroup>
               <Label for="lastName">Softskills training, medicals, security check..</Label>
-              <Input type="text" name="number" value={softSkillsPlusOthers} onChange={(e) => setSoftSkillsPlusOthers(e.target.value)} required />
+              <Input type="text" name="number" value={softSkillsPlusOthersLoan} onChange={(e) => setSoftSkillsPlusOthersLoan(e.target.value)} required />
             </FormGroup>
             <FormGroup>
               <Label for="lastName">LASDRI</Label>
-              <Input type="text" name="number" value={lasdri} onChange={(e) => setLasdri(e.target.value)} required />
+              <Input type="text" name="number" value={lasdriLoan} onChange={(e) => setLasdriLoan(e.target.value)} required />
             </FormGroup>
             <FormGroup>
               <Label for="lastName">DMS Subscription</Label>
-              <Input type="text" name="number" value={dmsSub} onChange={(e) => setDmsSub(e.target.value)} required />
+              <Input type="text" name="number" value={dmsSubLoan} onChange={(e) => setDmsSubLoan(e.target.value)} required />
             </FormGroup>
             <FormGroup>
               <Label for="lastName">E-taxi License</Label>
-              <Input type="text" name="number" value={eTaxi} onChange={(e) => setEtaxi(e.target.value)} required />
+              <Input type="text" name="number" value={eTaxiLoan} onChange={(e) => setEtaxiLoan(e.target.value)} required />
             </FormGroup>
           </ModalBody>
           <ModalFooter>
