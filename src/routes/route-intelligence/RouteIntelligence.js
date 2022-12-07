971bc386303f05e40e466845b207e5d2b232ce0c
Akinnawo3 <akinnawovictor@gmail.com>
2022-12-07 12:02:54 +0100

    cc

--- src/routes/route-intelligence/RouteIntelligence.js
+++ src/routes/route-intelligence/RouteIntelligence.js
@@ -2,13 +2,14 @@ import React from "react";
 import PageTitleBar from "Components/PageTitleBar/PageTitleBar";
 import RctCollapsibleCard from "Components/RctCollapsibleCard/RctCollapsibleCard";
 import { Card } from "reactstrap";
+import SummaryCards from "./component/summaryCards";
 
-const RouteIntelligence = ({match}) => {
+const RouteIntelligence = ({ match }) => {
   return (
     <div className="table-wrapper">
       <PageTitleBar title={"Route Intelligence"} match={match} />
       <Card>
-        
+        <SummaryCards />
       </Card>
     </div>
   );
--- src/routes/route-intelligence/component/summaryCards.js
+++ src/routes/route-intelligence/component/summaryCards.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col } from "reactstrap";
 
 const SummaryCards = () => {
   const items = [
@@ -6,13 +7,19 @@ const SummaryCards = () => {
     { title: "Total schedules", value: "3,123,456,789" },
     { title: "Total trip request", value: "3,123,456,789" },
     { title: "Total driver not found", value: "3,123,456,789" },
-];
-  return <div>
+  ];
+  return (
     <div>
-
+      <Col>
+        {items.map((item) => (
+          <div key={item?.title} className="p-3">
+            <h2>{item.value}</h2>
+            <div>{item.title}</div>
+          </div>
+        ))}
+      </Col>
     </div>
-  </div>;
+  );
 };
 
 export default SummaryCards;
-
