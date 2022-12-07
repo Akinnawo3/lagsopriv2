ae8c00c1ba778b9f7380374f507f19ab92bc9bff
Akinnawo3 <akinnawovictor@gmail.com>
2022-12-07 11:50:07 +0100

    cards

--- src/routes/route-intelligence/component/summaryCards.js
+++ src/routes/route-intelligence/component/summaryCards.js
@@ -4,9 +4,14 @@ const SummaryCards = () => {
   const items = [
     { title: "Total downloads", value: "3,123,456,789" },
     { title: "Total schedules", value: "3,123,456,789" },
+    { title: "Total trip request", value: "3,123,456,789" },
     { title: "Total driver not found", value: "3,123,456,789" },
 ];
-  return <div>SummaryCards</div>;
+  return <div>
+    <div>
+
+    </div>
+  </div>;
 };
 
 export default SummaryCards;
