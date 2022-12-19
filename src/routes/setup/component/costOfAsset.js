1b83055a8dbb22b32846a0ad101ed529b5ed0504
Increase Nkanta <engr.increase@gmail.com>
2022-12-19 22:39:04 +0100

    new build

--- src/routes/setup/component/costOfAsset.js
+++ src/routes/setup/component/costOfAsset.js
@@ -1,90 +1,91 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import RctCollapsibleCard from "Components/RctCollapsibleCard/RctCollapsibleCard";
-import {Form, FormGroup, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input, Button} from "reactstrap";
-import {Badge, Card, CardBody, Col, Row} from "reactstrap";
-import {connect} from "react-redux";
-import {createCostOfAsset, getCustomerCare} from "Actions/customerCareAction";
-import {verifyUserPermssion} from "../../../container/DefaultLayout";
+import { Form, FormGroup, Modal, ModalHeader, ModalBody, ModalFooter, Label, Input, Button } from "reactstrap";
+import { Badge, Card, CardBody, Col, Row } from "reactstrap";
+import { connect } from "react-redux";
+import { createCostOfAsset, getCustomerCare } from "Actions/customerCareAction";
+import { verifyUserPermssion } from "../../../container/DefaultLayout";
 
-const CostOfAsset = ({getCustomerCare, customerCareNumbers, createCostOfAsset}) => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [amount, setAmount] = useState("");
-  const updateAsset = () => {
-    setAmount(customerCareNumbers?.asset_amount);
-    setModalOpen(true);
-  };
-  useEffect(() => {
-    getCustomerCare(true);
-  }, []);
+const CostOfAsset = ({ getCustomerCare, customerCareNumbers, createCostOfAsset }) => {
+   const [modalOpen, setModalOpen] = useState(false);
+   const [amount, setAmount] = useState("");
+   const updateAsset = () => {
+      setAmount(customerCareNumbers?.asset_amount);
+      setModalOpen(true);
+   };
+   useEffect(() => {
+      getCustomerCare(true);
+   }, []);
 
-  const updateCostOfAsset = (e) => {
-    e.preventDefault();
-    setModalOpen(false);
-    createCostOfAsset(amount);
-  };
+   const updateCostOfAsset = (e) => {
+      e.preventDefault();
+      setModalOpen(false);
+      createCostOfAsset(amount);
+   };
 
-  console.log(customerCareNumbers?.asset_amount);
+   return (
+      <div className="row">
+         <div className="col col-xs-12 col-md-9">
+            <RctCollapsibleCard heading="Total Cost of Asset" style={{ minHeight: "70vh" }}>
+               <div className="px-2">
+                  <div className="row">
+                     <div className="cl col-sm-12 col-md-6">
+                        <p className="mt-2">
+                           <a href="https://docs.google.com/spreadsheets/d/157zYlbU_y_AciM2piJzcoZVfKX38_cTXICcBvhq9RoY/edit#gid=279137178" target="_blank">Click to see payment breakdown</a>
+                        </p>
+                        <Card className="text-primary bg-light p-2 mb-4 w-75">
+                           <CardBody className="pb-0">
+                              <div className="text-value text-left text-muted fw-bold">Total </div>
+                           </CardBody>
+                           <div className="chart-wrapper mx-3" style={{ height: "60px" }}>
+                              <span className="pr-2 font-xl" style={{ fontSize: "2.5rem" }}>
+                                 {` ₦${parseInt(customerCareNumbers?.asset_amount, 10)?.toLocaleString()}`}
+                              </span>
+                           </div>
+                        </Card>
 
-  return (
-    <div className="row">
-      <div className="col col-xs-12 col-md-9">
-        <RctCollapsibleCard heading="Total Cost of Asset" style={{minHeight: "70vh"}}>
-          <div className="px-2">
-            <div className="row mt-4">
-              <div className="cl col-sm-12 col-md-6">
-                <Card className="text-primary bg-light p-2 mb-4 w-75">
-                  <CardBody className="pb-0">
-                    <div className="text-value text-left text-muted fw-bold">Total </div>
-                  </CardBody>
-                  <div className="chart-wrapper mx-3 d-flex align-items-center justify-content-between" style={{height: "60px"}}>
-                    <span className="pr-2 font-xl" style={{fontSize: "2.5rem"}}>
-                      {` ₦${parseInt(customerCareNumbers?.asset_amount, 10)?.toLocaleString()}`}
-                    </span>
+                        <div classsName="d-flex mt-3">
+                           <button className="btn btn-info " onClick={() => verifyUserPermssion("create_setup", updateAsset)}>
+                              Update Fee
+                           </button>
+                        </div>
+                     </div>
                   </div>
-                </Card>
+               </div>
+            </RctCollapsibleCard>
+         </div>
 
-                <div classsName="d-flex mt-3">
-                  <button className="btn btn-info " onClick={() => verifyUserPermssion("create_setup", updateAsset)}>
-                    Update Fee
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </RctCollapsibleCard>
+         <Modal isOpen={modalOpen} toggle={() => setModalOpen(false)} size="sm">
+            <ModalHeader toggle={() => setModalOpen(false)}>Update Fee</ModalHeader>
+            <Form onSubmit={updateCostOfAsset}>
+               <ModalBody>
+                  <FormGroup>
+                     <Label for="lastName">Amount</Label>
+                     <Input type="text" name="number" value={amount} onChange={(e) => setAmount(e.target.value)} required />
+                  </FormGroup>
+               </ModalBody>
+               <ModalFooter>
+                  <Button type="submit" variant="contained" className="text-white btn-info mr-2">
+                     Update
+                  </Button>
+                  <Button variant="contained" className="btn btn-outline-danger" onClick={() => setModalOpen(false)}>
+                     Cancel
+                  </Button>
+               </ModalFooter>
+            </Form>
+         </Modal>
       </div>
-
-      <Modal isOpen={modalOpen} toggle={() => setModalOpen(false)} size="sm">
-        <ModalHeader toggle={() => setModalOpen(false)}>Update Fee</ModalHeader>
-        <Form onSubmit={updateCostOfAsset}>
-          <ModalBody>
-            <FormGroup>
-              <Label for="lastName">Amount</Label>
-              <Input type="text" name="number" value={amount} onChange={(e) => setAmount(e.target.value)} required />
-            </FormGroup>
-          </ModalBody>
-          <ModalFooter>
-            <Button type="submit" variant="contained" className="text-white btn-info mr-2">
-              Update
-            </Button>
-            <Button variant="contained" className="btn btn-outline-danger" onClick={() => setModalOpen(false)}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </Form>
-      </Modal>
-    </div>
-  );
+   );
 };
 
 function mapDispatchToProps(dispatch) {
-  return {
-    getCustomerCare: (spinner) => dispatch(getCustomerCare(spinner)),
-    createCostOfAsset: (asset_amount) => dispatch(createCostOfAsset(asset_amount)),
-  };
+   return {
+      getCustomerCare: (spinner) => dispatch(getCustomerCare(spinner)),
+      createCostOfAsset: (asset_amount) => dispatch(createCostOfAsset(asset_amount)),
+   };
 }
 
 const mapStateToProps = (state) => ({
-  customerCareNumbers: state.customerCare.customerCareNumbers,
+   customerCareNumbers: state.customerCare.customerCareNumbers,
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CostOfAsset);
